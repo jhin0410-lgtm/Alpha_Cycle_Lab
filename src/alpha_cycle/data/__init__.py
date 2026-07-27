9c601ff8a738e83bbfbb41e1802e0d571cafd9c8
jhin0410-lgtm <jhin0410@gmail.com>
2026-07-28 03:02:06 +0900

    Export revision-aware research data contracts

--- src/alpha_cycle/data/__init__.py
+++ src/alpha_cycle/data/__init__.py
@@ -1,4 +1,4 @@
-"""Market, point-in-time, and integrity data contracts."""
+"""Market, point-in-time, integrity, and research data contracts."""
 
 from alpha_cycle.data.integrity import (
     CorporateAction,
@@ -11,17 +11,41 @@ from alpha_cycle.data.integrity import (
 )
 from alpha_cycle.data.market import MarketDataFeed, ValidationReport, validate_ohlcv
 from alpha_cycle.data.point_in_time import PointInTimeStore, validate_point_in_time
+from alpha_cycle.data.research import (
+    CsvFinancialDataAdapter,
+    CsvMacroDataAdapter,
+    FinancialDataAdapter,
+    FinancialStatementStore,
+    MacroDataAdapter,
+    MacroSeriesStore,
+    ResearchDataPortal,
+    ResearchSnapshot,
+    RevisionPolicy,
+    validate_financial_statements,
+    validate_macro_series,
+)
 
 __all__ = [
     "CorporateAction",
     "CorporateActionStore",
     "CorporateActionType",
+    "CsvFinancialDataAdapter",
+    "CsvMacroDataAdapter",
+    "FinancialDataAdapter",
+    "FinancialStatementStore",
+    "MacroDataAdapter",
+    "MacroSeriesStore",
     "MarketDataFeed",
     "PointInTimeStore",
     "PriceBasis",
+    "ResearchDataPortal",
+    "ResearchSnapshot",
+    "RevisionPolicy",
     "UniverseMembershipStore",
     "ValidationReport",
     "validate_corporate_actions",
+    "validate_financial_statements",
+    "validate_macro_series",
     "validate_ohlcv",
     "validate_point_in_time",
     "validate_universe_membership",
