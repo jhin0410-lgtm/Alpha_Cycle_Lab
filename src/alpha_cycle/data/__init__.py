2b62bece933221e71e18b76b954584c63872e7ef
jhin0410-lgtm <jhin0410@gmail.com>
2026-07-28 01:56:31 +0900

    Export market integrity contracts

--- src/alpha_cycle/data/__init__.py
+++ src/alpha_cycle/data/__init__.py
@@ -1,13 +1,28 @@
-"""Market and point-in-time data contracts."""
+"""Market, point-in-time, and integrity data contracts."""
 
+from alpha_cycle.data.integrity import (
+    CorporateAction,
+    CorporateActionStore,
+    CorporateActionType,
+    PriceBasis,
+    UniverseMembershipStore,
+    validate_corporate_actions,
+    validate_universe_membership,
+)
 from alpha_cycle.data.market import MarketDataFeed, ValidationReport, validate_ohlcv
 from alpha_cycle.data.point_in_time import PointInTimeStore, validate_point_in_time
 
 __all__ = [
+    "CorporateAction",
+    "CorporateActionStore",
+    "CorporateActionType",
     "MarketDataFeed",
     "PointInTimeStore",
+    "PriceBasis",
+    "UniverseMembershipStore",
     "ValidationReport",
+    "validate_corporate_actions",
     "validate_ohlcv",
     "validate_point_in_time",
+    "validate_universe_membership",
 ]
-
