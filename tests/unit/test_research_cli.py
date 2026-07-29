e804a41a7c8e9aca64920ccc3e98675c95cc2b4a
jhin0410-lgtm <jhin0410@gmail.com>
2026-07-29 10:59:11 +0900

    Extend research CLI stock code tests

--- tests/unit/test_research_cli.py
+++ tests/unit/test_research_cli.py
@@ -10,10 +10,11 @@ from alpha_cycle.research_cli import _stock_codes
 def test_stock_codes_preserve_and_restore_leading_zeroes() -> None:
     assert _stock_codes("005930,000660") == ["005930", "000660"]
     assert _stock_codes("5930,660") == ["005930", "000660"]
+    assert _stock_codes("A005930,A000660") == ["005930", "000660"]
 
 
 def test_stock_codes_deduplicate_after_normalization() -> None:
-    assert _stock_codes("005930,5930") == ["005930"]
+    assert _stock_codes("005930,5930,A005930") == ["005930"]
 
 
 @pytest.mark.parametrize("value", ["", "AAPL", "1234567", "005930,AAPL"])
