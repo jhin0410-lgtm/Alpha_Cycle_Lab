3c4c7884fc003ec1d5d50fb0f41e061c272d702b
jhin0410-lgtm <jhin0410@gmail.com>
2026-08-04 12:47:23 +0900

    Run live pipeline with 64-bit project Python

--- scripts/run_live_pipeline.cmd
+++ scripts/run_live_pipeline.cmd
@@ -1,14 +1,6 @@
 @echo off
 setlocal
 chcp 65001 >nul
-for %%I in ("%~dp0..") do set "REPOSITORY_ROOT=%%~fI"
-pushd "%REPOSITORY_ROOT%" >nul || exit /b 2
-if defined PYTHONPATH (
-    set "PYTHONPATH=%REPOSITORY_ROOT%\src;%PYTHONPATH%"
-) else (
-    set "PYTHONPATH=%REPOSITORY_ROOT%\src"
-)
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0run_live_pipeline.ps1" %*
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0run_live_pipeline_bootstrap.ps1" %*
 set "EXIT_CODE=%ERRORLEVEL%"
-popd
 endlocal & exit /b %EXIT_CODE%
