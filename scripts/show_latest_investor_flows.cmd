2e399d103e3670810b14ea17a81acde2e55464a9
jhin0410-lgtm <jhin0410@gmail.com>
2026-08-08 20:31:46 +0900

    Add investor-flow diagnostics launcher

---
+++ scripts/show_latest_investor_flows.cmd
@@ -0,0 +1,6 @@
+@echo off
+setlocal
+chcp 65001 >nul
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0run_alpha_cycle_module.ps1" -Module "alpha_cycle.investor_flow_diagnostics_cli" %*
+set "EXIT_CODE=%ERRORLEVEL%"
+endlocal & exit /b %EXIT_CODE%
