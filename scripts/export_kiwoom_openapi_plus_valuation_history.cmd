48b8004584158a6b41a49a1e19c52a220ceb7d8f
jhin0410-lgtm <jhin0410@gmail.com>
2026-08-10 15:32:16 +0900

    Add cmd wrapper for Kiwoom valuation history

---
+++ scripts/export_kiwoom_openapi_plus_valuation_history.cmd
@@ -0,0 +1,6 @@
+@echo off
+setlocal
+chcp 65001 >nul
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0export_kiwoom_openapi_plus_valuation_history.ps1" %*
+set EXIT_CODE=%ERRORLEVEL%
+endlocal & exit /b %EXIT_CODE%
